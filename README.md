5b51d61c04ac4e40827c5a238a0ddb0c83a64c96
Arihant Jain <arihantjain9614@gmail.com>
2024-09-10 19:55:39 +0530

    gameLogic() function is complete now and windows size is changed to 20x20

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
 # Snake_Game
-This is a classic snake game built on top of C++. It's can be played on CMD. 
+This is a classic snake game built on top of C++. It's can be played on CMD.
 
 There are three fucntions currently ->
-    1. gameSetup() -> This initializes my game state.
-    2. Box() -> This function draws the layout everytime in repeat. Its the basic layout of the game and has the snake and fruit updated everytime.
-    3. KeyBoard_IP() -> This function accepts the keyboard input.
+1. gameSetup() -> This initializes my game state.
+2. Box() -> This function draws the layout everytime in repeat. Its the basic layout of the game and has the snake and fruit updated everytime.
+3. KeyBoard_IP() -> This function accepts the keyboard input.
 
 The important function - gameLogic() is left to be written.
 
-To run this file you will g++ version 12.2.0
+To run this file you will need g++ version 12.2.0
 
 Next steps ->
 Write the gameLogic() function.
-Write the functions in seperate files
-Test a makefile for this if possible
+Write the functions in seperate files.
+Add a new fruit which is of 5 points and appears after every 5 normal fruit and appears only for 7 seconds.
+Test a makefile for this if possible. 
--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@ enum Direction { Stop=0, Right, Left, Up, Down };
 
 int current_x, current_y; //snake's head postion
 int score=0; //keeping track of score collected
-const int Window_height=30;
-const int Window_width=30;
+const int Window_height=20; // changed to 20 from 30
+const int Window_width=20; // changed to 20 from 30
 int fruit_x,fruit_y; // fruits current postion
 Direction dir; // current direction update 
 
@@ -36,10 +36,10 @@ void Box(){
             if(j==0){
                 std::cout<<"#";
             }
-            if(i==current_x && j==current_y){
+            if(i==current_y && j==current_x){
                 std::cout<<"O"; //snake's head
             }
-            else if(i==fruit_x && j==fruit_y){
+            else if(i==fruit_y && j==fruit_x){
                 std::cout<<"1"; //fruit denoted with score the snake will get once collected
             }
             else{
@@ -90,7 +90,58 @@ void KeyBoard_IP(){
 }
 
 void gameLogic(){
-
+    int X = tail_x[0];
+    int Y = tail_y[0];
+    int a,b;
+    tail_x[0] = current_x;
+    tail_y[0] = current_y;
+    for(int i=1;i<tail_len;i++){
+        a = tail_x[i];
+        b = tail_y[i];
+        tail_x[i] = X;
+        tail_y[i] = Y;
+        X = a;
+        Y = b;
+    }
+    switch(dir){
+        case Up:
+            current_y--;
+            break;
+        case Right:
+            current_x++;
+            break;
+        case Down:
+            current_y++;
+            break;
+        case Left:
+            current_x--;
+            break;
+        default:
+            break;
+    }
+    if(current_x<0){
+        current_x = Window_width-1;
+    }
+    if(current_x>=Window_width){
+        current_x = 0;
+    }
+    if(current_y<0){
+        current_y = Window_height-1;
+    }
+    if(current_y>=Window_height){
+        current_y = 0;
+    }
+    for(int i=0;i<tail_len;i++){
+        if(tail_x[i]==current_x && tail_y[i]==current_y){
+            gameOver = true;
+        }
+    }
+    if(current_x == fruit_x && current_y == fruit_y){
+        score+=1;
+        fruit_x = rand()%Window_width;
+        fruit_y = rand()%Window_height;
+        tail_len+=1;
+    }
 }
 
 void gameSetup(){
